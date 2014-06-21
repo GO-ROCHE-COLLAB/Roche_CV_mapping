5edac6d8a83c57d23c3c89f326f49991da662be9
David Osumi-Sutherland <dosumis@gmail.com>
2014-06-21 20:28:23 +0100

    go path now comes from env var

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 export PATH="$PATH:../src"
-generate_mappings.py 'file:///repos/go_trunk_ont/extensions/go-plus.owl'
+generate_mappings.py $GO_PATH
+#'file:///repos/go_trunk_ont/extensions/go-plus.owl'
