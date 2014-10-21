be2650c2ed1e7c8c59d763b90330f32f354f0349
David Osumi-Sutherland <dosumis@gmail.com>
2014-10-21 10:33:42 +0100

    reconfigured to work with (dead simple) design patterns

--- src/mapping_tools.py
+++ src/mapping_tools.py
@@ -1,43 +1,47 @@
 #!/usr/bin/env jython -J-Xmx8000m
 from uk.ac.ebi.brain.core import Brain
 import re
+from pattern import gen_applied_pattern_from_json
 # TODO - switch from Brain to owltools
 
 def load_ont(url):
 	ont = Brain()
 	ont.learn(url)
 	return ont
-	
+
+
 # def brain2graphWrapper(ont):
 # 	onto = ont.getOntology()
 # 	return OWLGraphWrapper(onto)
 
-def roll_basic_pattern (key_class):
-	return "(has_participant some) %s or (regulates some (has_particpant some %s))" % (key_class, key_class) # If using this pattern, will need OWLtools/HermiT.
+#def roll_basic_pattern (key_class):
+#	return "(has_participant some) %s or (regulates some (has_particpant some %s))" % (key_class, key_class) # If using this pattern, will need OWLtools/HermiT.
 
 class map_obj:
 	# Should this work on ids or names?  Seems wasteful to store both.
 	def __str__(self):
 		return "Roche_cvt: %s; class_expression %s; manual_list_count %d, generated_list_count %d" % (self.RCV_id, self.class_expression, len(self.manual_list), len(self.generated_list))
-	def __init__ (self, go, RCV_id, manual_map, owl_map):
+	def __init__ (self, go, RCV_id, manual_map, owl_map, pattern_path):
 		# Key on ID. Lookup is responsibility of calling script.
 		# check go is a brain OR owl-api ontology object
 		"""Initialise map object: go = a Brain ontology object, 'RCV_ID' is the Roche term ID, manual_map is the mapping table as a list of dicts, keyed on column, owl_map is a row_column_dict of the owl mapping table."""
-		self.key_term = ''
-		self.class_expression = '' # Class expression used to generate list
 		self.manual_list = [] # Old manually curated mapping from Roche
 		self.generated_list = [] # Results of running OWL queries 
 		self.blacklist = []  # Terms blacklisted by Roche annotators
 		self.id_name = {}  # hash lookup for names of GO terms in lists (should this really be bodged in here?!)
 		"""go = go ontology object; RochCVterm = a single term from RocheCV; manual_map = colDict of manual map; owl_map = ColRowDict of owl mapping file"""
 		self.RCV_id = RCV_id
-		self.class_expression = owl_map[RCV_id]['class expression IDs']
+		self.pattern_name = owl_map[RCV_id]['Applied pattern']
+		dc = { 'key_class': ( owl_map[RCV_id]['Key Class Name'], owl_map[RCV_id]['Key Class ID'] )}
+		self.appl_pattern  = gen_applied_pattern_from_json(pattern_path + self.pattern_name + ".json", dc, go)
+		self.class_expression = self.appl_pattern.equivalentTo
 		for m in manual_map:
 			if m['RCV_ID'] == RCV_id:
 				self.manual_list.append(m['GO_ID'])
 		self.update_map(go)
 		self.update_id_name(go)
 		
+	
 		
 	def update_map(self, go):
 		"""Updates mapping generated from owl class expression"""
